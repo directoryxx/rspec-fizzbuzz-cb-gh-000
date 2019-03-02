e888313aaa6b312e6e376a4a99f178ea796112a9
Anak Agung Angga Wijaya <anggawijaya4567@gmail.com>
2019-03-02 18:18:34 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-required './spec/fizzbuzz_spec.rb'
+require './spec/fizzbuzz_spec.rb'
 def fizzbuzz(input)
   if input % 15 == 0
     "FizzBuzz"
